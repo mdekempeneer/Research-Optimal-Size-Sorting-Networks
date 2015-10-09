704eb52204a8dcdb19f001290a1812e88dc50709
mdekempeneer <mathiasdekempeneer@gmail.com>
2015-10-09 13:41:17 +0200

    Changes

--- SortingNetworksTester/src/networklib/Comparator.java
+++ SortingNetworksTester/src/networklib/Comparator.java
@@ -1,5 +1,7 @@
 package networklib;
 
+import tester.Bit;
+
 /**
  * A Comparator is used to cover 2 different channels and is able to compare the
  * value given over those 2 channels and sort them in an ascending order.
@@ -18,14 +20,14 @@ public class Comparator {
      * @param nbChannel2 The number of the second channel this covers.
      */
     public Comparator(short nbChannel1, short nbChannel2) {
-        this.nbChannel1 = nbChannel1;
-        this.nbChannel2 = nbChannel2;
+        this.nbChannel1 = (nbChannel1 < nbChannel2) ? nbChannel1 : nbChannel2;
+        this.nbChannel2 = (nbChannel1 < nbChannel2) ? nbChannel2 : nbChannel1;
     }
 
     /**
      * Get the number of the first channel this covers.
      *
-     * @return The number of channel1.
+     * @return The number of channel1, the lowest channelnumber.
      */
     public short getChannel1() {
         return this.nbChannel1;
@@ -34,12 +36,27 @@ public class Comparator {
     /**
      * Get the number of the second channel this covers.
      *
-     * @return The number of channel2.
+     * @return The number of channel2, the highest channelnumber.
      */
     public short getChannel2() {
         return this.nbChannel2;
     }
 
+    /**
+     * Swap the bits of channel1 and channel2 if necessary.
+     * making bit of channel1 less than or equal to channel2.
+     * @param input The input we 'sort' in.
+     */
+    public void swap(Bit[] input) {
+        Bit bit1 = input[nbChannel1 - 1];
+        Bit bit2 = input[nbChannel2 - 1];
+        if(bit1.getValue() > bit2.getValue()) {
+            int tmp = bit1.getValue();
+            bit1.setValue(bit2.getValue());
+            bit2.setValue(tmp);
+        }
+    }
+    
     /**
      * Returns a string representation of this {@link Comparator}.
      *
--- SortingNetworksTester/src/tester/Main.java
+++ SortingNetworksTester/src/tester/Main.java
@@ -14,40 +14,62 @@ import networklib.Network;
  * @author Mathias
  */
 public class Main {
-    
+
     public static boolean isSortingNetwork(Network network) {
+        /* Create input */
         int nbChannels = network.getNbChannels();
-        BitSet input = new BitSet(nbChannels);
+        Bit[] input = new Bit[nbChannels];
         for (int i = 0; i < nbChannels; i++) {
-            
+            input[i] = new Bit(0);
         }
-        
-        return false;
-        
+
+        for (int i = 0; i < nbChannels; i++) {
+            input[i].setValue(1); //TODO 000 moeten we checken
+
+            //test j = i+1
+            
+            for (int j = i + 2; j < nbChannels; j++) {
+                input[j - 1].setValue(0);
+                input[j].setValue(1);
+
+                System.out.println("");
+                for (Bit b : input) {
+                    System.out.print(b.getValue());
+                }
+
+                Bit[] output = network.getOutput(input);
+                if (Misc.isSorted(output) == false) {
+                    return false;
+                }
+
+            }
+        }
+
+        return true;
+
     }
-    
+
     public static void main(String[] args) {
         Network network = null;
         BitSet input = new BitSet(5);
-        input.set(2,true);
+        input.set(2, true);
         System.out.println(input.get(2));
-        
+
         if (args.length >= 1) {
             if (args[0].equals("-f")) {
                 //File
-               String line = Misc.readFile(args[1]);
-               network = Network.stringToNetwork(line);
-            }
-            else if (args[0].equals("-n")) {
+                String line = Misc.readFile(args[1]);
+                network = Network.stringToNetwork(line);
+            } else if (args[0].equals("-n")) {
                 //Args
                 int nbChannel = Integer.parseInt(args[1]);
                 int nbComp = Integer.parseInt(args[2]);
                 network = Network.stringToNetwork(nbChannel, nbComp, args[3]);
             }
         }
-        
+
         if (network != null) {
-            
+
         }
     }
 }
