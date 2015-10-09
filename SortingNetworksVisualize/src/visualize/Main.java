bd077e64f2e8a0b011e8426ea7c9d9e7aae8d40f
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2015-10-09 11:01:25 +0200

    * Pasted some code for reading a txt file.

--- SortingNetworksVisualize/src/visualize/Main.java
+++ SortingNetworksVisualize/src/visualize/Main.java
@@ -1,5 +1,7 @@
 package visualize;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import networklib.Network;
 
 /**
@@ -16,8 +18,20 @@ public class Main {
     public static void main(String args[]) {
         /* Analyse Args */
         //TODO: Analyse Args (-f filePath || -n n k (a,b)(c,d)(
+        if (args.length > 1) {
+            
+        }
         /* Parse & create Network */
-        //TODO: Parse 
+        
+        /*try (BufferedReader br = new BufferedReader(new FileReader("file.txt"))) { //TODO: Change to inputFile
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            if (line != null) {
+                Parser.parse(line);
+            }
+        }*/
+
         //Test Input
         Network network = new Network(4, 8);
         network.addComparator(1, 2);
