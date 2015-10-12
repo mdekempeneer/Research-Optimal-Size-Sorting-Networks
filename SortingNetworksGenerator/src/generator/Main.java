f50b1a997dee781ef10da1f4add938ac93d45c0e
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2015-10-12 10:25:33 +0200

    Added reading of arguments and using Generator.

--- SortingNetworksGenerator/src/generator/Main.java
+++ SortingNetworksGenerator/src/generator/Main.java
@@ -1,5 +1,7 @@
 package generator;
 
+import java.io.File;
+import javax.swing.JFileChooser;
 import networklib.Comparator;
 
 /**
@@ -7,35 +9,39 @@ import networklib.Comparator;
  * @author Admin
  */
 public class Main {
-    
+
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         if (args.length == 2) {
             int nbChannels = Integer.parseInt(args[0]);
             int nbComp = Integer.parseInt(args[1]);
-            
-            generate(nbChannels, nbComp);
-            
-            
+            String outputPath;
+
+            /* JFileChooser to choose the outputPath */
+            JFileChooser jfc = new JFileChooser();
+            jfc.setCurrentDirectory(new File(System.getProperty("user.home")));
+            int result = jfc.showOpenDialog(null);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                /* Generate */
+                outputPath = jfc.getSelectedFile().getAbsolutePath();
+                Generator gen = new Generator(nbChannels, nbComp, outputPath);
+                gen.generate();
+            } else {
+                System.out.println("Failed chosing a file.");
+            }
+        } else if (args.length == 3) {
+            int nbChannels = Integer.parseInt(args[0]);
+            int nbComp = Integer.parseInt(args[1]);
+            String outputPath = args[2];
+
+            /* Generate */
+            Generator gen = new Generator(nbChannels, nbComp, outputPath);
+            gen.generate();
         } else {
             System.out.println("Format is: n k");
-        }
-    }
-    
-    public static void generate(int channels, int max) {
-        Comparator[] comps = new Comparator[max];
-        
-        //Iterate  over all comparator combination
-        for (int number1 = 1; number1 <= channels-1; number1++) {
-            for (int number2 = number1+1; number2 <= channels; number2++) {
-            generate_sub(comps, new Comparator(number1, number2), 0, channels);
-        }
-        }
-    }
-    
-    private static void generate_sub(Comparator[] list, Comparator comp, int current, int nbChannels) {
-        int max = list.length;
-        if (current < max) {
-            Comparator[] newList = cloneCompList(list);
         }
     }
 }
