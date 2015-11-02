a6984346596c3b5f32682b5843d6ba0a5088ab3d
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2015-11-02 16:54:34 +0100

    * Uses generator3

--- SortingNetworksGenerator/src/generator/Main.java
+++ SortingNetworksGenerator/src/generator/Main.java
@@ -26,7 +26,7 @@ public class Main {
             if (result == JFileChooser.APPROVE_OPTION) {
                 /* Generate */
                 outputPath = jfc.getSelectedFile().getAbsolutePath();
-                Generator2 gen = new Generator2(nbChannels, nbComp, outputPath);
+                Generator3 gen = new Generator3(nbChannels, nbComp, outputPath);
                 gen.generate();
             } else {
                 System.out.println("Failed chosing a file.");
@@ -37,7 +37,7 @@ public class Main {
             String outputPath = args[2];
 
             /* Generate */
-            Generator2 gen = new Generator2(nbChannels, nbComp, outputPath);
+            Generator3 gen = new Generator3(nbChannels, nbComp, outputPath);
             gen.generate();
         } else {
             System.out.println("Format is: n k or n k outputPath");
