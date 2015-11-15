22fb3762a11d35dea462cc81d712580b5db53dd9
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2015-11-15 15:53:23 +0100

    Added the first Generation step

--- SortingNetworksPaper/src/sortingnetworkspaper/Processor.java
+++ SortingNetworksPaper/src/sortingnetworkspaper/Processor.java
@@ -1,7 +1,7 @@
 package sortingnetworkspaper;
 
-import org.paukov.combinatorics.Factory;
-import org.paukov.combinatorics.combination.simple.SimpleCombinationGenerator;
+import it.unimi.dsi.fastutil.objects.ObjectBigArrayBigList;
+import it.unimi.dsi.fastutil.shorts.ShortOpenHashSet;
 
 /**
  *
@@ -12,20 +12,80 @@ import org.paukov.combinatorics.combination.simple.SimpleCombinationGenerator;
  */
 public class Processor {
 
-    short nbChannels;
+    private final short nbChannels;
+    private final int upperBound;
+    private ObjectBigArrayBigList<short[][]> N;
 
-    public Processor(short nbChannels) {
+    
+    /**
+     * 
+     * @param nbChannels
+     * @param upperBound 
+     */
+    public Processor(short nbChannels, int upperBound) {
         this.nbChannels = nbChannels;
+        this.upperBound = upperBound;
+        this.N = new ObjectBigArrayBigList();
     }
 
-    public short[] process(int upperBound) {
+    /**
+     * 
+     * @return 
+     */
+    public short[] process() {
+        /* Initialize inputs */
         short[][] inputs = getOriginalInputs(upperBound);
+        firstTimeGenerate(inputs);
+        
+        //While (upperBound niet bereikt en Size(N) != 1: GENERATE & PRUNE
 
         return null;
     }
-    
-    public void generate() {
+
+    /**
+     * Add all networks consisting of 1 comparator to N.
+     */
+    private void firstTimeGenerate(short[][] inputs) {
+        int maxX = ((1 << (nbChannels - 1)) | 1);
+        int maxShifts = nbChannels - 2; //# opschuiven
+        short comp;
+        int number;
+        int outerShift;
+
+        /* For all comparators */
+        for (number = 3; number <= maxX; number = (number << 1) - 1, maxShifts--) { //x*2 - 1
+            comp = (short) number;
+            for (outerShift = 0; outerShift <= maxShifts; outerShift++, comp <<= 1) { //shift n-2, n-3, ... keer
+                //new Network (via clone)
+                short[][] data = inputs.clone();
+                //Fill
+                data[0] = new short[upperBound];
+                data[0][0] = comp;
+                processData(data, comp);
+                
+                N.add(data);
+            }
+        }
+    }
+
+    /**
+     * Processes the data for the new comparator. Adding the comparator to the
+     * data[0] is assumed to be done already.
+     */
+    private void processData(short[][] data, short newComp) {
+        ShortOpenHashSet set = new ShortOpenHashSet();
         
+        for (int i = 1; i < data.length; i++) { //For all #1'en
+            set.clear();
+            for(int j = 0; j < data[i].length; j++) {
+                set.add(swapCompare(data[i][j], newComp)); //Add comp(output)
+            }
+            data[i] = set.toShortArray();
+        }
+    }
+
+    private void generate() {
+
     }
 
     /**
@@ -53,16 +113,15 @@ public class Processor {
      * ones.
      */
     private short[][] getOriginalInputs(int upperBound) {
-        //TODO: data[0] should probably hold comparators so far.
         /* 
          data[0] holds the lengths of the other shorts.
          data[1] holds outputs with 1 '1's.
          data[2] holds outputs with 2 '1's.
          ...
          */
-        short[][] data = new short[nbChannels][]; //TODO: nbChannels-1; 1111 niet nuttig
+        short[][] data = new short[nbChannels][];
         data[0] = new short[upperBound];
-        
+
         for (int numberOfOnes = 1; numberOfOnes < nbChannels; numberOfOnes++) {
             data[numberOfOnes] = getPermutations((short) ((1 << numberOfOnes) - 1), nbChannels);
         }
@@ -71,6 +130,23 @@ public class Processor {
     }
 
     /**
+     * Get the output of the comparator comp given the input.
+     *
+     * @param input The input to give the comparator.
+     * @param comp The comparator to get the output from.
+     * @return The result by switching the bits in the input according to comp.
+     */
+    private static short swapCompare(short input, short comp) {
+        int pos1 = 31 - Integer.numberOfLeadingZeros(comp);
+        int pos2 = Integer.numberOfTrailingZeros(comp);
+
+        //(input >> pos1) & 1 = first (front bit)
+        //(input >> pos2) & 1 = 2nd (back bit)
+        return (((input >> pos1) & 1) <= ((input >> pos2) & 1)) ? input : (short) (input ^ comp);// TADAM!!!
+    }
+
+    /**
+     * TODO: Possible to optimize
      *
      * @param n
      * @return
