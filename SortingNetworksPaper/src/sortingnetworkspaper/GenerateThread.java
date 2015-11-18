1fbeb9231a9c16714fb2dfafc2dfcf2b9c750004
mdekempeneer <mathiasdekempeneer@gmail.com>
2015-11-18 13:05:12 +0100

    Bug fix

--- SortingNetworksPaper/src/sortingnetworkspaper/GenerateThread.java
+++ SortingNetworksPaper/src/sortingnetworkspaper/GenerateThread.java
@@ -12,50 +12,56 @@ public class GenerateThread implements Runnable {
     private ObjectBigArrayBigList<short[][]> partN;
     private final Processor p;
     private final int maxX;
-    private int maxShifts;
+    private int maxShiftsStored;
     private short nbComp;
     private long startIndex;
     private long endIndex;
+    private short[] allPosComp;
 
-    GenerateThread(Processor p) {
+    GenerateThread(Processor p, short[] allPosComp) {
         partN = new ObjectBigArrayBigList();
         maxX = ((1 << (p.getNbChannels() - 1)) | 1);
-        maxShifts = p.getNbChannels() - 2;
+        maxShiftsStored = p.getNbChannels() - 2;
         this.p = p;
+        this.allPosComp = allPosComp;
     }
 
     @Override
     public void run() {
         int number;
         int outerShift;
-        int comp;
+//        int comp;
+        int maxShifts;
         ObjectBigArrayBigList<short[][]> N = p.getN();
         long index = startIndex;
-        ObjectBigListIterator<short[][]> iter = N.listIterator(index);
+        ObjectBigListIterator<short[][]> iter = N.listIterator(index); //lastIndex problem
 
         partN.clear();
         short[][] network;
         short[][] tempNetwork;
         while (iter.hasNext() && index < endIndex) { //For all networks
             network = iter.next();
-            //For all comps
-            for (number = 3; number <= maxX; number = (number << 1) - 1, maxShifts--) { //x*2 - 1
-                comp = number;
-                for (outerShift = 0; outerShift <= maxShifts; outerShift++, comp <<= 1) { //shift n-2, n-3, ... keer
-                    tempNetwork = network.clone();
-                    tempNetwork[0] = network[0].clone();
-                    tempNetwork[0][nbComp] = (short) comp; //TODO: 1 Cast better?
-                    Processor.processData(tempNetwork, (short) comp);
-                    partN.add(tempNetwork);
-                }
+            for (short comp : allPosComp) {
+                tempNetwork = network.clone();
+                tempNetwork[0] = network[0].clone();
+                tempNetwork[0][nbComp] = (short) comp; //TODO: 1 Cast better?
+                Processor.processData(tempNetwork, (short) comp);
+                partN.add(tempNetwork);
             }
+//            //For all comps
+//            for (number = 3, maxShifts = maxShiftsStored; number <= maxX; number = (number << 1) - 1, maxShifts--) { //x*2 - 1
+//                comp = number;
+//                for (outerShift = 0; outerShift <= maxShifts; outerShift++, comp <<= 1) { //shift n-2, n-3, ... keer
+//                    tempNetwork = network.clone();
+//                    tempNetwork[0] = network[0].clone();
+//                    tempNetwork[0][nbComp] = (short) comp; //TODO: 1 Cast better?
+//                    Processor.processData(tempNetwork, (short) comp);
+//                    partN.add(tempNetwork);
+//                }
+//            }
             index++;
         }
         p.addToNewN(partN);
-        
-        synchronized (p) {
-            p.notify(); //TODO: Add within addToNewN to prevent another synchronized layer?
-        }
     }
 
     /**
