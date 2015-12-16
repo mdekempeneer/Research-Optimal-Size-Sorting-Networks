3b8cae30529a0e3c2f94c857d72173c0dc433e9c
mdekempeneer <mathiasdekempeneer@gmail.com>
2015-12-16 12:15:56 +0100

    Updated tests of the Single Processor Class

--- SortingNetworksPaper/test/sortingnetworkspaper/SingleProcessorTest.java
+++ SortingNetworksPaper/test/sortingnetworkspaper/SingleProcessorTest.java
@@ -38,11 +38,6 @@ public class SingleProcessorTest {
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
     @Test
     public void testProcessW() {
         int upperBound = 5;
