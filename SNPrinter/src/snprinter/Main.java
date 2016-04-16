8f0de04bb2920fa774b282c124f4891b6eb36246
VincentDerk <vincent.derkinderen@student.kuleuven.be>
2016-04-16 13:56:34 +0200

    Added printing permutations.

--- SNPrinter/src/snprinter/Main.java
+++ SNPrinter/src/snprinter/Main.java
@@ -15,22 +15,41 @@ public class Main {
      */
     public static void main(String[] args) {
         nbChannels = Short.parseShort(args[0]);
+        /* Parse arg 1 */
         String[] temp = args[1].split(",");
-        input = new short[temp.length];
+        input1 = new short[temp.length];
 
         for (int i = 0; i < temp.length; i++) {
-            input[i] = Short.parseShort(temp[i]);
+            input1[i] = Short.parseShort(temp[i]);
+        }
+        /* Parse arg 2 */
+        if (args.length >= 3) {
+            temp = args[2].split(",");
+            input2 = new short[temp.length];
+
+            for (int i = 0; i < temp.length; i++) {
+                input2[i] = Short.parseShort(temp[i]);
+            }
+        }
+        /* Action */
+        if (args.length >= 3) {
+            data1 = process(input1);
+            System.out.println("next network");
+            data2 = process(input2);
+            printPermList(data1, data2);
+        } else if (input1.length >= 1) {
+            process(input1);
         }
 
-        if (input.length >= 1) {
-            process();
-        }
     }
 
-    private static short[] input;
+    private static short[] input1;
+    private static short[] input2;
+    private static short[][] data1;
+    private static short[][] data2;
     private static short nbChannels;
 
-    public static void process() {
+    public static short[][] process(short[] input) {
         short[][] defaultNetwork = getOriginalInputs(0);
 
         short[][] data = defaultNetwork.clone();
@@ -48,8 +67,10 @@ public class Main {
 
             printData(data);
         }
+        return data;
     }
 
+    /* --- Printing --- */
     public static void printData(short[][] data) {
         System.out.println("Data: ");
         for (int i = 1; i < data.length; i++) {
@@ -57,20 +78,119 @@ public class Main {
         }
         System.out.println("");
     }
-    
+
     public static String printBinary(short[] data) {
         StringBuilder str = new StringBuilder();
         str.append("[");
-        for(int i = 0; i < data.length; i++) {
+        for (int i = 0; i < data.length; i++) {
             String number = String.format("%5s", Integer.toBinaryString(data[i])).replace(' ', '0');
             //String = Integer.toBinaryString(data[i])
             str.append(number).append(",");
         }
-        str.deleteCharAt(str.length()-1);
+        str.deleteCharAt(str.length() - 1);
         str.append("]");
         return str.toString();
     }
 
+    /* --- Permutations --- */
+    public static void printPermList(short[][] network1, short[][] network2) {
+        //Create a list full of allOnes - on every Position can be every number.
+        int allOnes = (1 << nbChannels) - 1;
+        int[] posList = new int[nbChannels];
+        System.arraycopy(Main.getAllOnesList(nbChannels), 0, posList, 0, nbChannels);
+
+        //Compute positions.
+        for (int nbOnes = 1; nbOnes < nbChannels; nbOnes++) {
+            int L2 = network2[nbChannels][(nbOnes << 2) - 2]; //L for network 2.
+            int P2 = network2[nbChannels][(nbOnes - 1) << 2]; //P for network 2.
+
+            int revLPos = allOnes ^ network1[nbChannels][(nbOnes << 2) - 2]; //Positions of 0 for L.
+            int revPPos = allOnes ^ network1[nbChannels][(nbOnes - 1) << 2]; //Positions of 1 for P.
+
+            //TODO: Can we shorten result = revOnePos & revZeroPos by inline + logic?
+            //and combine the two if's in the process.
+            //int result = revLPos & revPPos;
+            //Fill posList;
+            if (L2 != allOnes || P2 != allOnes) {
+                for (int i = 0; i < posList.length; i++) {
+                    int mask = 1 << i;
+                    if ((mask & L2) == 0) {
+                        posList[i] &= revLPos;
+                    }
+                    if ((mask & P2) == 0) {
+                        posList[i] &= revPPos;
+                    }
+
+                    //If possible positions are ever empty -> return false.
+                    if (posList[i] == 0) {
+                        //emptyPosCounter++;
+                        return;
+                    }
+                }
+            }
+        }
+
+        /* If at certain position only 1 number possible,
+           claim the number and remove from other positions.
+         */
+        for (int i = 0; i < posList.length; i++) {
+            int value = posList[i];
+            if (Integer.bitCount(value) == 1) {
+                for (int j = 0; j < posList.length; j++) {
+                    if (((value & posList[j]) != 0) && (i != j)) {
+                        posList[j] -= value;
+                        if (posList[j] == 0) {
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        /* Convert posList bit structure to bytes for permutations. */
+        byte[][] Ps = new byte[nbChannels][];
+
+        //TODO: Can this structure be improved?
+        for (int i = 0; i < Ps.length; i++) {
+            byte[] tempP = new byte[nbChannels]; //don't know initial capacity required.
+            int countLengthPos = 0;
+            int currP = posList[i];
+
+            //Retrieve possible numbers from the bit form (currP).
+            for (byte permIndex = 0; permIndex < nbChannels; permIndex++) {
+                if ((1 << permIndex & currP) != 0) {// mask & posList[i] == 1 op die positie.
+                    tempP[countLengthPos++] = permIndex;
+                }
+            }
+
+            Ps[i] = new byte[countLengthPos]; //trim down to appropriate sizes.
+            System.arraycopy(tempP, 0, Ps[i], 0, countLengthPos);
+        }
+
+        //Print all permutations.
+        for (int j = 0; j < Ps.length; j++) {
+            System.out.println("On place " + j + ": " + Arrays.toString(Ps[j]));
+        }
+    }
+
+    /**
+     * Create an array of nbChannels elements equal to allOnes. allOnes = (1
+     * &lt&lt nbChannels) - 1
+     *
+     * @param nbChannels The amount of the elements and the amount of 1's.
+     * @return The created array.
+     */
+    private static int[] getAllOnesList(short nbChannels) {
+        int allOnes = (1 << nbChannels) - 1;
+        int[] list = new int[nbChannels];
+
+        for (int i = 0; i < list.length; i++) {
+            list[i] = allOnes;
+        }
+        return list;
+    }
+
+    /* --- Generation --- */
     /**
      * Get all original inputs excluding the already sorted ones.
      *
