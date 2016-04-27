cd361a2060cad96a901ebbe6d8aeb8622af23983
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-04-27 11:14:53 +0200

    benadering berekening

--- Paper/compare10channels.m
+++ Paper/compare10channels.m
@@ -1,36 +1,40 @@
 clc
+clear all
 %%
-old   = [6 13 21 44 102 159 391 3436 56887 1237241 26637970 489689647]; % ms
-old_s = old ./ 1000; % s
-old_m = old_s ./ 60; % min
-old_u = old_m ./ 60; % uur
-old_d = old_u ./ 24; % dag
-old_totaal_dag = sum(old_d);
+xas             = 2:29;
+old             = zeros([1 28]);
+old(1:12)       = [6 13 21 44 102 159 391 3436 56887 1237241 26637970 489689647]; % ms
+old_s           = old ./ 1000; % s
+old_m           = old_s ./ 60; % min
+old_u           = old_m ./ 60; % uur
+old_d           = old_u ./ 24; % dag
+old_totaal_dag  = sum(old_d);
 
-new   = [26 12 20 42 96 410 1147 4875 54304 1079551 22804439 0]; % ms
-new_s = new ./ 1000; % s
-new_m = new_s ./ 60; % min
-new_u = new_m ./ 60; % uur
-new_d = new_u ./ 24; % dag
-new_totaal_dag = sum(new_d);
+new             = zeros([1 28]);
+new(1:11)       = [26 12 20 42 96 410 1147 4875 54304 1079551 22804439]; % ms
+new_s           = new ./ 1000; % s
+new_m           = new_s ./ 60; % min
+new_u           = new_m ./ 60; % uur
+new_d           = new_u ./ 24; % dag
+new_totaal_dag  = sum(new_d);
 
 %%
 figure
-plot(2:13, old, '*-')
+plot(2:13, old(1:12), '*-')
 xlabel('comparator')
 ylabel('miliseconden')
 set(gca, 'Yscale', 'log')
 hold on
-plot(2:13, new, 'o-')
+plot(2:12, new(1:11), 'o-')
 
 %%
 figure
-plot(2:13, old_u, '*-')
+plot(2:13, old_u(1:12), '*-')
 xlabel('comparator')
 ylabel('uren')
 set(gca, 'Yscale', 'log')
 hold on
-plot(2:13, new_u, 'o-')
+plot(2:12, new_u(1:11), 'o-')
 
 %%
 diff = (old_m(1:11) - new_m(1:11));
@@ -46,5 +50,29 @@ hold on
 plot(2:12, cumdiff, 'o')
 
 %%
-bar(2:13, [old_m; new_m]')
+figure
+bar(xas, [old_m; new_m]')
 set(gca, 'Yscale', 'log')
+
+%%
+
+T  = table(xas(1:12), old(1:12));
+T2 = table(xas(1:11), new(1:11));
+figure
+plot(xas(1:12),old(1:12),'o')
+hold on
+plot(xas(1:11),new(1:11),'o')
+set(gca,'YScale','log')
+[p, ~,mu ] = polyfit(T.Var1,  T.Var2,  11);
+[p2,~,mu2] = polyfit(T2.Var1, T2.Var2, 10);
+f  = polyval(p,  xas, [], mu);
+f2 = polyval(p2, xas, [], mu2);
+plot(xas,f)
+plot(xas,f2)
+ylabel('nanoseconden')
+xlabel('comparator')
+legend('old', 'new', 'benadering old', 'benadering new', 'Location', 'northwest')
+
+%%
+benadering_uur = f2(12) / (1000 * 60 * 60)
+benadering_dag = benadering_uur /24
