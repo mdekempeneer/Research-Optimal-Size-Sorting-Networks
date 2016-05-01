a13318090a4b25c1a90400ab96743d74e7b8089f
Mathias Dekempeneer <mathiasdekempeneer@gmail.com>
2016-05-01 14:39:11 +0200

    Benaderingen

--- Paper/compare10channels.m
+++ Paper/compare10channels.m
@@ -89,10 +89,47 @@ plot(xas(1:11),new_d(1:11),'o')
 set(gca,'YScale','log')
 [p3,~,mu3] = polyfit(T3.Var1, T3.Var2, 11);
 [p4,~,mu4] = polyfit(T4.Var1, T4.Var2, 10);
-f3 = polyval(p3, xas(1:14), [], mu3);
-f4 = polyval(p4, xas(1:14), [], mu4);
-plot(xas(1:14),f3)
-plot(xas(1:14),f4)
+f3 = polyval(p3, xas(1:15), [], mu3);
+f4 = polyval(p4, xas(1:15), [], mu4);
+plot(xas(1:15),f3)
+plot(xas(1:15),f4)
 ylabel('dagen')
 xlabel('comparator')
 legend('old', 'new', 'benadering old', 'benadering new', 'Location', 'northwest')
+
+%%
+old(17:28) = fliplr(old(1:12));
+
+old_s           = old ./ 1000; % s
+old_m           = old_s ./ 60; % min
+old_u           = old_m ./ 60; % uur
+old_d           = old_u ./ 24; % dag
+
+new(18:28) = fliplr(new(1:11));
+
+new_s           = new ./ 1000; % s
+new_m           = new_s ./ 60; % min
+new_u           = new_m ./ 60; % uur
+new_d           = new_u ./ 24; % dag
+
+T5 = table(xas(17:28), old_d(17:28));
+T6 = table(xas(18:28), new_d(18:28));
+figure
+plot(xas(17:28),old_d(17:28),'ro')
+hold on
+plot(xas(18:28),new_d(18:28),'bo')
+plot(xas(1:11),new_d(1:11),'bo')
+plot(xas(1:12),old_d(1:12),'ro')
+set(gca,'YScale','log')
+grid on
+[p5,~,mu5] = polyfit(T5.Var1, T5.Var2, 11);
+[p6,~,mu6] = polyfit(T6.Var1, T6.Var2, 10);
+f5 = polyval(p5, xas, [], mu5);
+f6 = polyval(p6, xas, [], mu6);
+plot(xas(1:15),f3, 'r')
+plot(xas(1:15),f4, 'b')
+plot(xas(14:28),f5(14:28), 'r')
+plot(xas(14:28),f6(14:28), 'b')
+% ylabel('dagen')
+% xlabel('comparator')
+% legend('old', 'new', 'benadering old', 'benadering new', 'Location', 'northwest')
